f1c3dc0bed490d58a619b663fe9931ba5cc4c9de
Cristiano Junior <cristianonpj@gmail.com>
2022-07-04 19:43:40 -0300

    post styles (part 1)

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -15,10 +15,10 @@ export function Post(){
                 </div>
             </header>
             <div className={styles.content}>
-            <p>Fala galeraa 👋</p>
-            <p>Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀</p>
-            <p>👉<a href=""> jane.design/doctorcare</a></p>
-            <p><a>#novoprojeto #nlw #rocketseat</a></p>
+                <p>Fala galeraa 👋</p>
+                <p>Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀</p>
+                <p>👉<a href=""> jane.design/doctorcare</a></p>
+                <p><a>#novoprojeto #nlw #rocketseat</a></p>
             </div>
         </article>
     )
